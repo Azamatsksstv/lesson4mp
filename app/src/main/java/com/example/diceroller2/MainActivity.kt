11040387996e816f473801738965aa706ff1fd91
Azamat Zhambylbayev <200103392@stu.sdu.edu.kz>
2022-09-28 12:48:32 +0500

    6. Add the dice roll logic - Create a rollDice() method

--- app/src/main/java/com/example/diceroller2/MainActivity.kt
+++ app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -13,8 +13,18 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
