5c36c82daaec2d0b52267a35c9e003b241b9b091
Azamat Zhambylbayev <200103392@stu.sdu.edu.kz>
2022-09-28 13:27:21 +0500

    6. Adopt good coding practices - Comment Your Code

--- app/src/main/java/com/example/diceroller2/MainActivity.kt
+++ app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -27,11 +27,14 @@ class MainActivity : AppCompatActivity() {
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
+        // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
 
+        // Determine which drawable resource ID to use based on the dice roll
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -40,9 +43,12 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.contentDescription = diceRoll.toString()
 
+        // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
